f97440f38c48282f9fc716bab5ee9db95cd4a4bb
Arnold Jabea <aroljabea71@gmail.com>
2024-09-18 05:15:02 +0200

    initial commit for new-button

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -14,6 +14,8 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
     this.title = 'Haman';
@@ -21,5 +23,27 @@ export class FaceSnapComponent implements OnInit {
     this.createdAt = new Date();
     this.snaps = 0;
     this.imageUrl = 'https://images.unsplash.com/photo-1652636347412-46f47856f540?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    this.snapButtonText = 'oh snap!';
+    this.userHasSnapped = false;
   }
+  onSnap(): void {
+    if (this.userHasSnapped) {this.unSnap();
+  
+    } else {
+      this.snap();
+  
+    }
+
+  }
+    unSnap(): void {
+      this.snaps--;
+      this.snapButtonText = 'oh snap!';
+      this.userHasSnapped = false;
+    }
+    snap(): void {
+      this.snaps++;
+      this.snapButtonText = 'Oops, snap!';
+      this.userHasSnapped = true;
+    }
+
 }
