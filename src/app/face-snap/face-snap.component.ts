0abe0f41ab511e101990b42550a381fb8555d5af
Arnold Jabea <aroljabea71@gmail.com>
2024-09-18 04:11:49 +0200

    Added ImageCard component to display images with title and description

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit(): void {
+    this.title = 'Haman';
+    this.description = 'Is my best friend';
+    this.createdAt = new Date();
+    this.snaps = 0;
+    this.imageUrl = 'https://images.unsplash.com/photo-1652636347412-46f47856f540?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  }
+}
