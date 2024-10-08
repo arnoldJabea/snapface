c4bc94d053c5c4862e1f7e84cb1a6df1655828aa
Arnold Jabea <aroljabea71@gmail.com>
2024-10-08 04:17:24 +0200

    add the array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snaps';
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,36 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+
+
+  ngOnInit(): void {
+    this.faceSnaps = [
+      this.mySnap = new FaceSnap(
+        'Haman', 'Is my best friend', 'https://images.unsplash.com/photo-1652636347412-46f47856f540?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', new Date(), 12
+      ),
+
+      this.myOtherSnap = new FaceSnap(
+        'Sport', 'my favorite sport', 'https://images.unsplash.com/photo-1461896836934-ffe607ba8211?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', new Date(), 2
+      ),
+
+      
+      this.myLastSnap = new FaceSnap(
+        'photograph', 'The photo', 'https://images.unsplash.com/photo-1593644311729-d0b478c97d95?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', new Date(),
+        12
+      )
+
+    ];
+
+
+
+    this.faceSnaps[0].setLocation('Paris');
+
+
+
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, input, OnInit } from '@angular/core';
+import { FaceSnap } from '../models/face-snaps';
 
 @Component({
   selector: 'app-face-snap',
@@ -8,21 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './face-snap.component.scss'
 })
 export class FaceSnapComponent implements OnInit {
+ @Input() faceSnap!: FaceSnap;
 
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
+ 
   snapButtonText!: string;
   userHasSnapped!: boolean;
 
   ngOnInit(): void {
-    this.title = 'Haman';
-    this.description = 'Is my best friend';
-    this.createdAt = new Date();
-    this.snaps = 0;
-    this.imageUrl = 'https://images.unsplash.com/photo-1652636347412-46f47856f540?q=80&w=2668&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    
     this.snapButtonText = 'oh snap!';
     this.userHasSnapped = false;
   }
@@ -36,12 +30,12 @@ export class FaceSnapComponent implements OnInit {
 
   }
     unSnap(): void {
-      this.snaps--;
+      this.faceSnap.removeSnap();
       this.snapButtonText = 'oh snap!';
       this.userHasSnapped = false;
     }
     snap(): void {
-      this.snaps++;
+      this.faceSnap.addSnap();
       this.snapButtonText = 'Oops, snap!';
       this.userHasSnapped = true;
     }
